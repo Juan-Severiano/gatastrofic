29f3c9a6b105ae17116d9ca76ebc0da7ebea31f5
juan <juansoussev@gmail.com>
2023-07-13 20:58:10 -0300

    asd

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ pass.addEventListener('keyup', () => {
     }
     if ((pass.value.toLowerCase() === 'igor')) {
         msgSuccess.classList.remove('d-none')
+        window.location.href = 'index2.html'
         addDNone()
     }
     else if ((pass.value.toLowerCase() === 'saiko')) {
@@ -49,3 +50,15 @@ pass.addEventListener('keyup', () => {
         msgError.classList.add('d-none')
     }
 })
+
+
+window.addEventListener('scroll', function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var parallaxElements = document.getElementsByClassName('parallax-element');
+
+    for (var i = 0; i < parallaxElements.length; i++) {
+        var speed = parallaxElements[i].getAttribute('data-speed');
+        var yPos = -(scrollTop * speed);
+        parallaxElements[i].style.transform = 'translate3d(0px, ' + yPos + 'px, 0px)';
+    }
+});
